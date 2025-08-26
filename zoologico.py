5a89380f221dce6c10941f3901430a8de17930d0
Ana Julia Boneti Volgue <ana.volgue@escola.pr.gov.br>
2025-08-26 12:18:05 +0000

    zoo prt1

---
+++ zoologico.py
@@ -0,0 +1,11 @@
+zoo = int(input("digite um número de 1 a 10 que irá aparecer um animal do zoologico que você se parece"))
+if zoo == 1:
+    print("você se parece com um leão")
+elif zoo == 2:
+    print("vocẽ é um bixo preguiça")
+elif zoo == 3:
+    print("você é um gato")
+elif zoo == 4:
+    print("você é um cachorro")
+elif zoo == 5:
+    print("você é ")
